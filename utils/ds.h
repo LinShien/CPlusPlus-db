06be49c15c4ff975348dd5065d7c629269d17944
size7311 <size7311@gmail.com>
2021-07-27 18:30:40 +0800

    Deserialization problem needed to be solved

--- utils/ds.h
+++ utils/ds.h
@@ -8,11 +8,11 @@ const int COLUMN_EMAIL_SIZE = 255;
 
 /**
  * Simple and hardcoded row represent data structure
- * column	        type
+ * column	           type
  * 
- * id	            integer
+ * id	                       integer
  * username	        varchar(32)
- * email	        varchar(255)
+ * email	               varchar(255)
  */
 struct Row {
     int id;
@@ -23,25 +23,25 @@ struct Row {
 /**
  *  column	  size (bytes)	 offset
  * 
- *  id	      4	             0
- *  username  32             4
- *  email	  255	         36
- *  total	  291
+ *  id	                    4	                0
+ *  username   32                   4
+ *  email	        255	               36
+ *  total	          291
  */
 
-const int ID_SIZE = sizeof(((Row*) 0)->id);
-const int USERNAME_SIZE = sizeof(((Row*) 0)->username);
-const int EMAIL_SIZE = sizeof(((Row*) 0)->email);
-const int ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+const unsigned int ID_SIZE = sizeof(((Row*) 0)->id);
+const unsigned int USERNAME_SIZE = sizeof(((Row*) 0)->username);
+const unsigned int EMAIL_SIZE = sizeof(((Row*) 0)->email);
+const unsigned int ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
-const int ID_OFFSET = 0;
-const int USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const int EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const unsigned int ID_OFFSET = 0;
+const unsigned int USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const unsigned int EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 
-const int PAGE_SIZE = 4096;           // same as big as page in virtual memory
-const int TABLE_MAX_PAGES = 100;
-const int ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const int TABLE_MAX_ROWS = TABLE_MAX_PAGES * ROWS_PER_PAGE;
+const unsigned int PAGE_SIZE = 4096;           // same as big as page in virtual memory
+const unsigned int TABLE_MAX_PAGES = 100;
+const unsigned int ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const unsigned int TABLE_MAX_ROWS = TABLE_MAX_PAGES * ROWS_PER_PAGE;
 
 /**
  * A data structure to support data persistent on disk
--- utils/utils.cpp
+++ utils/utils.cpp
@@ -4,7 +4,6 @@
 #include <memory>
 #include <cstring>
 #include <fstream>
-#include <filesystem>
 
 void print_prompt() {
     std::cout << "c++db > ";
@@ -123,14 +122,17 @@ ExecuteResult execute_statement(const Statement& statement, Table& table) {
 
 void serialize_row(const Row* source, void* destination) {
     memcpy(((char*) destination) + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(((char*) destination) + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(((char*) destination) + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    // memcpy(((char*) destination) + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    // memcpy(((char*) destination) + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+
+    strncpy((char*) (destination + USERNAME_OFFSET), source ->username, USERNAME_SIZE);
+    strncpy((char*) (destination + EMAIL_OFFSET), source ->email, EMAIL_SIZE);
 }
 
-void deserialize_row(const void* source, Row* desination) {
-    memcpy(&(desination->id), ((char*) source) + ID_OFFSET, ID_SIZE);
-    memcpy(&(desination->username), ((char*) source) + USERNAME_OFFSET, USERNAME_SIZE);
-    memcpy(&(desination->email), ((char*) source) + EMAIL_OFFSET, EMAIL_SIZE);
+void deserialize_row(const void* source, Row* destination) {
+    memcpy(&(destination->id), ((char*) source) + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), ((char*) source) + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), ((char*) source) + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 void* fetch_row(Table& table, int row_num) {
